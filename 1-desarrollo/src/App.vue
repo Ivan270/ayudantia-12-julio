299d56ae4983a3c5ced53b53ec90cc7517861d40
Ivan Verdugo C. <ivan270@gmail.com>
2024-07-12 23:02:27 -0400

    ayudantia finalizada

--- 1-desarrollo/src/App.vue
+++ 1-desarrollo/src/App.vue
@@ -1,26 +1,122 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div class="container">
+      <div class="row justify-content-center mb-3 gap-4">
+        <!-- Usuario 1 -->
+        <div class="card col-2">
+          <img :src="firstUser?.picture.large" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{ firstUser?.name?.first }} {{ firstUser?.name?.last }}</h5>
+            <form id="primera"
+              @submit.prevent="sendMessageHandler(firstUser?.id?.value, firstUserMessage, firstUser?.name?.first, firstUser?.name?.last)">
+              <input class="form-control" type="color" name="color" id="color" v-model="firstUserColor">
+              <input class="form-control" type="text" name="message" id="message" v-model="firstUserMessage">
+              <div class="d-grid">
+                <button type="submit" class="btn btn-outline-light btn-sm">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <!-- Cuadro del chat -->
+        <div id="chatBox" class="border border-secondary p-2 col-5 rounded">
+          <ChatBox :messages="messages" :firstUser="firstUser" :secondUser="secondUser" />
+        </div>
+
+        <!-- Usuario 2 -->
+        <div class="card col-2">
+          <img :src="secondUser?.picture.large" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{ secondUser?.name?.first }} {{ secondUser?.name?.last }}</h5>
+            <form id="primera"
+              @submit.prevent="sendMessageHandler(secondUser?.id?.value, secondUserMessage, secondUser?.name?.first, secondUser?.name?.last)">
+              <input class="form-control" type="color" name="color" id="color" v-model="secondUserColor">
+              <input class="form-control" type="text" name="message" id="message" v-model="secondUserMessage">
+              <div class="d-grid">
+                <button type="submit" class="btn btn-outline-light btn-sm">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import ChatBox from '@/components/ChatBox.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      users: [],
+      firstUserColor: '#075E54',
+      firstUserMessage: '',
+      secondUserColor: '#128C7E',
+      secondUserMessage: '',
+      messages: []
+    }
+  },
+  methods: {
+    async buscarUsers() {
+      try {
+        let response = await axios.get('https://randomuser.me/api/?results=2');
+        this.users = response.data.results
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    sendMessageHandler(id, message, firstName, lastName) {
+      const newMessage = {
+        userId: id,
+        message: message,
+        color: id == this.firstUser.id.value ? this.firstUserColor : this.secondUserColor,
+        name: firstName,
+        last: lastName
+      }
+      this.messages.push(newMessage)
+      // Limpiar form
+      if (id == this.firstUser.id.value) {
+        this.firstUserMessage = ''
+      } else if (id == this.secondUser.id.value) {
+        this.secondUserMessage = ''
+      }
+    },
+  },
+  computed: {
+    firstUser() {
+      return this.users[0]
+    },
+    secondUser() {
+      return this.users[1]
+    }
+  },
   components: {
-    HelloWorld
+    ChatBox,
+  },
+  // Lifecycle hooks
+  created() {
+    this.buscarUsers();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#App{
+  min-height: 100vh;
+  display: flex;
+  place-items: center
 }
+
+#chatBox{
+  min-height: 50vh;
+  overflow-y: auto;
+  max-height: 50vh;
+}
+.card{
+  height: 100% !important;
+}
+
+
 </style>
---
+++ 1-desarrollo/src/components/ChatBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container mb-3">
+        <div class="d-flex flex-column" v-for="(message, index) in messages"
+            :class="message.userId == firstUser.id.value ? 'align-items-start' : message.userId == secondUser.id.value ? 'align-items-end' : ''"
+            :key="index">
+            <small class="time fw-light text-secondary mb-1">{{ message?.name }} {{ message?.last }}</small>
+            <div class="d-flex align-items-center">
+                <small class="time text-secondary">{{ currentTime }}</small>
+                <p :style="{ backgroundColor: message.color }">{{ message?.message }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ChatBox',
+    props: {
+        messages: Array,
+        firstUser: Object,
+        secondUser: Object
+    },
+    data: function () {
+        return {}
+    },
+    computed: {
+        currentTime() {
+            let date = new Date();
+            let time = date.toLocaleTimeString('es-CL', { hour: '2-digit', minute: '2-digit', hour12: false })
+            return time
+        }
+    }
+    ,
+    //methods: {},
+    // watch: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+.time {
+    font-size: 10px
+}
+</style>
